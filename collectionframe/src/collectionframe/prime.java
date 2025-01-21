b6e9cc40265afae45b11922291d712e7b3798168
Pushkar1803 <151548910+Pushkar1803@users.noreply.github.com>
2025-01-21 17:30:37 +0530

    prime.java

--- collectionframe/src/collectionframe/prime.java
+++ collectionframe/src/collectionframe/prime.java
@@ -1,18 +1,21 @@
-package collectionframe;
-
 public class prime {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-//		
-		for(int i=2;i<=100;i++) {
-			if (i/1==i && i/i==1 ){
-				System.out.println(i);
-			}
-			
-			
-		}
-	}
+    public static void main(String[] args) {
+        // Loop through numbers from 2 to 100
+        for (int i = 2; i <= 100; i++) {
+            int divisorCount = 0;
 
+            // Count the divisors of the number
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisorCount++;
+                }
+            }
+
+            // A number is prime if it has exactly 2 divisors (1 and itself)
+            if (divisorCount == 2) {
+                System.out.println(i);
+            }
+        }
+    }
 }
